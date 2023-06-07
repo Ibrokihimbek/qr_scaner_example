4f3a07f61290a99b47ef501ae6187bb4f3d02e06
Ibrohimbek Davronbekov <108320639+Ibrokihimbek@users.noreply.github.com>
2023-06-07 14:31:03 +0500

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_scaner_example/screens/home/home_page.dart';
+import 'package:qr_scaner_example/screens/timer/timer_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,86 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: Scaffold(body: StopwatchWidget(),),
     );
   }
 }
+
+
+/// Birinchi textfielddan ikkinchisiga o'tkazib yuborish
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: MyWidget(),
+//     );
+//   }
+// }
+
+// class MyWidget extends StatefulWidget {
+//   @override
+//   _MyWidgetState createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   late TextEditingController _firstTextFieldController;
+//   late TextEditingController _secondTextFieldController;
+//   late FocusNode _secondTextFieldFocusNode;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _firstTextFieldController = TextEditingController();
+//     _secondTextFieldController = TextEditingController();
+//     _secondTextFieldFocusNode = FocusNode();
+
+//     _firstTextFieldController.addListener(() {
+//       if (_firstTextFieldController.text.length == 19) {
+//         FocusScope.of(context).requestFocus(_secondTextFieldFocusNode);
+//       }
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _firstTextFieldController.dispose();
+//     _secondTextFieldController.dispose();
+//     _secondTextFieldFocusNode.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Column(
+//           children: [
+//             TextField(
+//               controller: _firstTextFieldController,
+//               decoration: InputDecoration(
+//                 labelText: 'First TextField',
+//               ),
+//             ),
+//             TextField(
+//               controller: _secondTextFieldController,
+//               focusNode: _secondTextFieldFocusNode,
+//               decoration: InputDecoration(
+//                 labelText: 'Second TextField',
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
